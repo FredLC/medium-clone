66fe216f619d9d195b4fc3da8a15e0f46ce7cc4e
Fred Lefebvre <fredlefevre@outlook.com>
2023-01-23 14:53:11 -0500

    Implemented header and banner components

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "links.papareact.com",
+      },
+      {
+        protocol: "https",
+        hostname: "accountabilitylab.org",
+      },
+    ],
+  },
+};
